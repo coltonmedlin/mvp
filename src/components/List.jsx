06d717cd691e4366b0f5283c53fe1d49d07f55a5
Colton Medlin <coltonmedlin@gmail.com>
2021-04-08 15:54:35 -0700

    - adding items with enter key - crossing and uncrossing items

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,14 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { hot } from 'react-hot-loader/root';
 
-const List = (props) => (
-  <div>
-    <ul>
-      {props.uncategorized.map((item, index) =>
-        <li key={index}>{item}</li>
-      )}
-    </ul>
-  </div>
-)
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      completed: []
+    };
+    this.markComplete = this.markComplete.bind(this);
+  }
+
+  markComplete(index) {
+    if (!this.state.completed.includes(index)) {
+      this.setState({completed: [...this.state.completed, index]});
+    } else {
+      let i = this.state.completed.indexOf(index);
+      let arr = this.state.completed;
+      delete arr[i];
+      this.setState({completed: arr});
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.props.uncategorized.map((item, index) =>
+            <li key={index} class={this.state.completed.includes(index) ? 'completed' : ''} onClick={() => {this.markComplete(index)}}>{item}</li>
+          )}
+        </ul>
+      </div>
+    )
+  }
+}
 
 export default hot(List);
